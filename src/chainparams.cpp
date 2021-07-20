95bdba3c98dbe0ab51d4d83809c7dbc0134178bc
Akshay <akshaycm@hotmail.com>
2021-07-20 10:47:00 +0300

    backport of particl/particl-core@8f4cc83

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -422,6 +422,9 @@ public:
         consensus.rct_time = 0x5D2DBC40;                // 2019-07-16 12:00:00
         consensus.smsg_difficulty_time = 0x5D2DBC40;    // 2019-07-16 12:00:00
 
+        consensus.m_frozen_anon_index = 27340;
+        consensus.m_frozen_blinded_height = 884433;
+
         consensus.smsg_fee_period = 5040;
         consensus.smsg_fee_funding_tx_per_k = 200000;
         consensus.smsg_fee_msg_per_day_per_k = 50000;
